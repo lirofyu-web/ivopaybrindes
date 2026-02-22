731c5b873b1391d3c1cb557988a3d187ce4ed848
mrd brindes <mrdbrindes2@gmail.com>
2026-02-22 16:00:18 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: ["https://6000-firebase-studio-1771768388840.cluster-ocv3ypmyqfbqysslgd7zlhmxek.cloudworkstations.dev"],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
