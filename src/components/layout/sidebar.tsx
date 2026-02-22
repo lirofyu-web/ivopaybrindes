51c2721c01406bc70b20a0b8ffea7ae44804e8cf
mrd brindes <mrdbrindes2@gmail.com>
2026-02-22 17:16:03 +0000

    quero testar o app em tela cheia

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -40,7 +40,7 @@ export default function AppSidebar() {
   return (
     <Sidebar
       className="border-r bg-sidebar print:hidden"
-      collapsible={isMobile ? 'offcanvas' : 'icon'}
+      collapsible="offcanvas"
     >
       <SidebarHeader className="p-4">
         <div className="flex items-center justify-between">
