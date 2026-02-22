55d4ec0c632f37a0796071debaea44b638df9dee
mrd brindes <mrdbrindes2@gmail.com>
2026-02-22 18:19:55 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,2 +1,9 @@
 // This file is populated by the backend with the Firebase project configuration.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "studio-6418989475-b2135",
+  "appId": "1:208666933548:web:b2594d63757bb7e8be9489",
+  "apiKey": "AIzaSyDJg5nOk0q7oTBoy7Xsg0vceF_BKHtTV5Y",
+  "authDomain": "studio-6418989475-b2135.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "208666933548"
+};
